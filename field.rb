33106ad6005fb7f47add8680e55f4c8fbdf99a82
amos03 <amossi@gmail.com>
2021-07-01 19:15:35 -0400

    got rid of farm.rb and instead just doing field.rb and main.rb

--- field.rb
+++ field.rb
@@ -1,7 +1,22 @@
 class Field
 @@fields=[]
+@@food_harvested=0
 
-    def initialize
-        @field_type
-        @field_size
+
+    def initialize(type,size)
+        @field_type=type
+        @field_size=size
+        @@fields << self
     end
+
+    def self.harvest
+    end
+    
+    def self.display_status
+    # display # of hectares of each field, i.e., each instance of field
+    end
+    
+    def self.relax
+        # vista strings that incorporate references to all field instances
+    end
+    
